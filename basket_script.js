b25b6436ac04a1a9da9939942e321666d742f10f
Dima Shkoda <dimashkoda2000@gmail.com>
2022-07-31 18:04:52 +0300

    second update, with div in output

--- basket_script.js
+++ basket_script.js
@@ -36,10 +36,19 @@ let tableWrapperBasket = document.querySelector('.table-wrapper');
 let basketTableBasket = document.querySelector('.basket-table');
 let basketTitleBasket = document.querySelector('.table-title');
 let costBasket = document.querySelector('.cost');
+let itemBasket;
 
 
-
-
+function showBasketDetails() {
+    let titleBasket = document.createElement('tr');
+    titleBasket.innerHTML = `
+        <td>№</td>
+        <td>Product</td>
+        <td>Quantity</td>
+        <td>Price</td>
+    
+    `
+    basketTableBasket.append(titleBasket)
     for (let i = 0; i < productsArrBasket.length; i++) {
         if (getLocalStorageBasket.length === 0) {
             basketTableBasket.style.display = 'none';
@@ -50,7 +59,9 @@ let costBasket = document.querySelector('.cost');
             tableWrapperBasket.append(elementEmptyBasket);
         } else {
             let priceResultBasket = getLocalStorageBasket[i].price * getLocalStorageBasket[i].quantity;
-            let itemBasket = document.createElement('tr');
+
+
+            itemBasket = document.createElement('tr');
             itemBasket.innerHTML = `
 
                 <td>${getLocalStorageBasket[i].id}.</td>
@@ -59,7 +70,7 @@ let costBasket = document.querySelector('.cost');
                     <button class="number-minus" type="button"
                             onclick="this.nextElementSibling.stepDown()">-
                     </button>
-                    <input type="number" placeholder='Enter quantity' min="1" value="${getLocalStorageBasket[i].quantity}">
+                    <input type="number" placeholder='Enter quantity' min="1" value="${getLocalStorageBasket[i].quantity}" data-set = '${getLocalStorageBasket[i].id}'>
                     <button class="number-plus" type="button"
                             onclick="this.previousElementSibling.stepUp()">
                         +
@@ -72,7 +83,86 @@ let costBasket = document.querySelector('.cost');
         }
 
     }
+    changeValueBasket();
+}
 
+showBasketDetails();
+
+function changeValueBasket() {
+    let input = document.querySelectorAll('input');
+    input.forEach(function (item, value) {
+        item.oninput = function () {
+            console.log(item.value);
+            getLocalStorageBasket[value].quantity = +item.value;
+            console.log(getLocalStorageBasket);
+            // document.querySelectorAll('tr').forEach(function (element) {
+            //     element.style.display = 'none'
+            // })
+            showBasketDetailsResult();
+        }
+    })
+}
+
+
+function showBasketDetailsResult() {
+
+    let emptyTrBasket = document.createElement('tr');
+    emptyTrBasket.className = 'empty-tr-basket';
+    emptyTrBasket.innerHTML = `
+    <td> </td>
+    <td></td>
+    <td> </td>
+    <td> </td>`;
+    basketTableBasket.append(emptyTrBasket);
+    let titleBasket = document.createElement('tr');
+    titleBasket.className = 'title-tr-basket';
+    titleBasket.innerHTML = `
+        <td>№</td>
+        <td>Product</td>
+        <td>Quantity</td>
+        <td>Price</td>
+    
+    `
+    basketTableBasket.append(titleBasket)
+    for (let i = 0; i < productsArrBasket.length; i++) {
+        if (getLocalStorageBasket.length === 0) {
+            basketTableBasket.style.display = 'none';
+            let elementEmptyBasket = document.createElement('div');
+            elementEmptyBasket.className = 'empty-basket';
+            elementEmptyBasket.innerHTML = 'The basket is empty.';
+            elementEmptyBasket.style.textAlign = 'center';
+            tableWrapperBasket.append(elementEmptyBasket);
+        } else {
+            let priceResultBasket = getLocalStorageBasket[i].price * getLocalStorageBasket[i].quantity;
+
+
+            itemBasket = document.createElement('tr');
+            itemBasket.className = 'item-tr-basket';
+            itemBasket.innerHTML = `
+
+                <td>${getLocalStorageBasket[i].id}.</td>
+                <td>${getLocalStorageBasket[i].name}</td>
+                <td class="input-td"><div class="counter-form-input">
+                    ${getLocalStorageBasket[i].quantity}
+                </div></td>
+                <td class="cost">${priceResultBasket.toFixed(2)}$</td>
+            
+    `;
+            basketTableBasket.append(itemBasket)
+        }
+
+    }
+    let resultBasket = document.createElement('tr');
+    resultBasket.className = 'result-tr-basket';
+    resultBasket.innerHTML = `
+                <td colspan="3" class="basket-table_total">Total:</td>
+                <td>${sum().toFixed(2)} $</td>
+`
+    basketTableBasket.append(resultBasket);
+}
+
+
+console.log(getLocalStorageBasket);
 
 function sum() {
     return getLocalStorageBasket.reduce((sum, itemBasket) => sum + itemBasket.price * itemBasket.quantity, 0);
@@ -85,10 +175,5 @@ function sum() {
 
 
 
-let resultBasket = document.createElement('tr');
-resultBasket.innerHTML = `
-                <td colspan="3" class="basket-table_total">Total:</td>
-                <td>${sum()}$</td>
-`
-basketTableBasket.append(resultBasket);
+
 
